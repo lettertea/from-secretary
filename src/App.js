60ec0ad9008fd84ed597788ecb35f490a47d785a
nashkenazy <34195263+nashkenazy@users.noreply.github.com>
2020-08-14 10:55:25 -0700

    Update article

--- src/App.js
+++ src/App.js
@@ -13,18 +13,20 @@ function App() {
             <p>If you absolutely need to find the best candidate in a limited pool, and if you can only judge
                 one candidate at a time and can't go back, consider the secretary algorithm. It uses an "optimal
                 stopping"
-                approach by always rejecting the first X amount of candidates before accepting a candidate. Done
+                approach by always rejecting the first X amount of candidates before accepting one. Done
                 correctly, that candidate will very likely be rank 1 or 2 of the entire pool.</p>
-            <p>But it depends on the stopping strategy. A popular one is the 1/e, or 36.8%, strategy. If
+            <p>But it depends on the stopping strategy. A popular one is the 36.8%, or "1/e", approach where the first
+                36.8% of the total candidates are rejected. If
                 we have 100 candidates to choose from, we would evaluate then reject the first 37 candidates. It doesn't
                 matter how good
                 they are. The rule is to always reject them. Then pick the first of the remaining 63 candidates that is
                 better than all of the 37 rejected.</p>
             <p>
-                This strategy is error-prone and restrictive, however. There's a 37% chance the best candidate of the
+                This strategy is failure-prone and restrictive, however. There's a 37% chance the best candidate of the
                 100
                 is within the first 37 rejected. That means we will get to the end of the candidate list without
-                choosing anyone since the criteria is to find an even better candidate who doesn't exist.</p>
+                choosing anyone because we would be trying to find a candidate better than the best. And the fact is
+                that at minimum, we have to reject 37 candidates.</p>
             <p>There are more lax strategies like the 25% approach or the sqrt(N) (e.g. total pool is 64, so reject
                 first 8) approach. The found candidate will be
                 ranked worse on average, but there's less chance of failing and the candidate will be found earlier on
@@ -39,8 +41,8 @@ function App() {
                 rank 2 would be okay. But then how about 3? And then 4? When should we stop?
             </p>
             <p>
-                When it comes to large population, the difference in ranks becomes quite minimal. The opposite is true
-                as well. In a small population, the difference in the candidate's percentile is large.
+                When it comes to large populations, the difference in ranks becomes quite minimal. The opposite is true
+                as well. In small populations, the difference in the candidate's percentile is large.
             </p>
             <AveragePercentile/>
             <Typography variant={"caption"}>Candidate Percentile Mean = (Total Population - (Candidate Rank - 1)) /
@@ -52,16 +54,18 @@ function App() {
             </p>
             <p>
                 The answer would depend on the population, but analyzing the data points of released SAT scores can
-                help. CollegeBoard used to release yearly SAT scores breakdowns of all SAT tests taken that year, and
+                help. CollegeBoard used to release detailed breakdowns of all SAT test scores achieved in the year every
+                year, and
                 the <Link
                 href="https://web.archive.org/web/20170106113421/https://secure-media.collegeboard.org/digitalServices/pdf/sat/sat-percentile-ranks-composite-crit-reading-math-writing-2015.pdf"
                 target="_blank" rel="noopener noreferrer">
                 2015 dataset
-            </Link> was the last of it. The distribution more or less follows a normal distribution. Most people tend
-                towards average, so the
-                difference
+            </Link> was the last of it. (They still release data yearly but with less information.) Every year though,
+                the
+                distribution more or less follows a normal distribution. Most people tend
+                towards average, so the difference
                 between, say, the 30 percentile and the 70 percentile isn't that much considering the large difference
-                in percentile. According to the data, it would be 350 points. And when approaching outliers, the
+                in percentile. According to the 2015 data, it would be 350 points. And when approaching outliers, the
                 difference per percentile is more significant.
                 The difference between the 98.02 percentile and the 99.98 percentile is 220 points.
             </p>
@@ -70,7 +74,8 @@ function App() {
                 returns.
                 It should be much harder to improve from scoring 2300 to 2400 compared to from 1400 to 1500. Yet just
                 going
-                up the percentile slightly at those outlier ranges, the scores jump a lot more compared to the range around the mean. I thought the
+                up the percentile slightly at those outlier ranges, the scores jump a lot more compared to the range
+                around the mean. I thought the
                 distribution would be like an upside-down parabola, where there the difference is significantly less at
                 the
                 outlier ranges. But at this
@@ -106,10 +111,12 @@ function App() {
                 The insightful thing about the secretary approach is the stopping rule. By evaluating some number
                 of candidates before deciding,
                 we get a better understanding of the candidates a particular behavior yields. This is
-                especially important when picking a single candidate in an unfamiliar field. This is the "review" portion of the chart below.
+                especially important when picking a single candidate in an unfamiliar field. This is the "review"
+                portion of the chart below.
             </p>
             <p>
-                The "planning" portion is more of a fun exercise in my opinion. I put less weight in it since a lot of it depends on
+                The "planning" portion is more of a fun exercise in my opinion. I put less weight in it since a lot of
+                it depends on
                 chance. But if you need to be very efficient when dedicating resources to evaluate candidates, it can be
                 useful.
             </p>

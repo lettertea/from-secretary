803312bad705e21420a3a7f1a40a605f3b55dc39
lettertea <34195263+lettertea@users.noreply.github.com>
2022-12-31 01:50:45 +0900

    Rewrite article

--- src/App.js
+++ src/App.js
@@ -12,152 +12,50 @@ function App() {
     return (
         <Typography className="App">
             <Typography variant={"h2"}>The Secretary Problem</Typography>
-            <p>If you absolutely need to find the best candidate in a limited pool, and if you can only judge one
-                candidate at a time and can't go back, consider the secretary algorithm. It uses an "optimal stopping"
-                approach by always rejecting the first X number of candidates before choosing one. Done correctly, that
-                candidate will very likely be rank 1 or 2 of the entire pool.
-            </p>
-            <p>The variation that will achieve that result is called the 36.8%, or "1/e" (‘e’ being Euler’s number),
-                approach where the first 36.8% of the total candidates are always rejected. If we have 100 candidates to
-                choose from, we would evaluate then reject the first 37 candidates. It doesn't matter how good they are.
-                The rule is to always reject them. Then pick the first of the remaining 63 candidates that is better
-                than all of the 37 rejected.</p>
-            <p>
-                This strategy is failure-prone and restrictive, however. There's a 37% chance the best candidate of the
-                100 is within the first 37 rejected. That means we will get to the end of the candidate list without
-                choosing anyone because we would be trying to find a candidate better than the best. And to make it even
-                worse, we have to reject at least 37 candidates—a waste of time and energy for everyone.</p>
-            <p>In contrast, there are more lax strategies like the 25% or sqrt(N) (e.g., total pool, N, is 64, so reject
-                the first 8) approach. The found candidate will be ranked worse on average, but there's less chance of
-                failing and the candidate will be found earlier on average.</p>
+            <p>It’s possible to find the very best candidate with about a 37% success rate within a large pool of candidates, whether a hundred or a trillion, with the secretary algorithm. The caveat is that we can only evaluate one candidate, must stop once a candidate has been chosen, and evaluate in a uniformly random order. It uses an "optimal stopping" approach where the first X number of candidates has to be rejected then chooses the first candidate that performs better than the X number of candidates rejected.</p>
+            <p>The variation that will achieve that result is called the 36.8%, or "1/e" (‘e’ being Euler’s number). Given a pool of 100 candidates, the first 37 are evaluated then rejected, regardless of their performance. Then the first candidate better than the 37 rejected from the remaining 63 is chosen.</p>
+            <p>This strategy is failure-prone and restrictive, however. There's a 37% chance the best candidate of the 100 is within the first 37 rejected. That means we will get to the end of the candidate list without choosing a candidate because we would be trying to find a candidate better than the best.</p>
+            <p>In contrast, there are more lax strategies like the 25% or sqrt(N) (e.g., total pool, N, is 64, so reject the first 8) approach. The chosen candidate will be worse on average, but there's less chance of failing and the candidate will be found earlier.</p>
+
             <SecretaryChart/>
             <p>
-                The sqrt(N) strategy is unique compared to fixed percentage approaches because of the way it adapts to
-                the population size. As the total pool increases, the stopping rule threshold barely increases. It does
-                not grow linearly. It reduces the burden of executing the secretary algorithm and does not compromise
-                candidate performance too much. This is because in large pools, or N, values, the overall percentile
-                difference between each candidate becomes quite minimal. The opposite is true as well. In small sample
-                populations, the difference in the candidate's percentile is large.
+            When working with large population sizes, the sqrt(N) strategy may be preferable since the stopping threshold would grow by root rather than linearly. Plus in large pools, the overall percentile difference between each candidate rank shrinks.
             </p>
             <AveragePercentile/>
             <Typography variant={"caption"}>Candidate Percentile Mean = (Total Population - (Candidate Rank - 1)) /
                 (Total
                 Population + 1)</Typography>
-            <p>
-                To help put these percentiles into context, analyzing SAT scores can help. CollegeBoard used
-                to release detailed breakdowns of all SAT test scores achieved in the year, and
-                the <Link
+                <h1>What is considered a good candidate?</h1>
+
+
+<p>I looked into SAT scores since it’s relatable to many students (particularly in the US) and that there’s a lot of data available to the public, like <Link
                 href="https://web.archive.org/web/20170106113421/https://secure-media.collegeboard.org/digitalServices/pdf/sat/sat-percentile-ranks-composite-crit-reading-math-writing-2015.pdf"
                 target="_blank" rel="noopener noreferrer">
-                2015 dataset
-            </Link> was the last of it (they still release data yearly but with less information).
-            </p>
-            <p>
-                Every year though,
-                the distribution more or less follows a normal distribution. Most people tend
-                towards the median, so the difference
-                between, say, the 30 percentile and the 70 percentile isn't that much considering the large difference
-                in percentile. According to the 2015 data, it would be 350 points. And when approaching outliers, the
-                difference per percentile is more significant.
-                The difference between the 98.02 percentile and the 99.98 percentile is 220 points despite only being
-                roughly 2 percentiles apart.
-            </p>
-            <p>
-                I struggled to accept this because I believed improvement followed the law of diminishing
-                returns.
-                It should have been much harder to improve from 2300 to 2400 compared to from 1400 to 1500. Yet just
-                going
-                up the percentile slightly at those outlier ranges, the scores jumped a lot more compared to the range
-                around the median. But at this point, I wasn't really questioning the
-                SAT data but more about why normal distributions can appear naturally.
-            </p>
-            <p>
-                If we roll a single die enough times, we will get a uniform distribution of results.
-                With two dice, it will look like a
-                triangle because there are more ways to achieve 7 than 2. And when we keep adding more dice, the
-                data points quickly begin to resemble a normal distribution. There are more combinations of rolls
-                that land closer to the median value than the ones at the ends.
-            </p>
-            <DiceChart/>
-            <p>
-                With the SAT score distribution, the explanation
-                may not be as simple as analyzing dice rolls combinations, but there are
-                similarities. Scoring around the median is more common because the requirements to score at that range
-                are flexible. Maybe someone is a good test taker but didn't study much. Maybe someone else is bad at
-                taking tests but studied a lot. There isn't a particular description you have to fit in order to score
-                near the median since many are acceptable.
-            </p>
-            <p>
-                But to score at the extreme outlier ranges, there isn't much leeway. You can't be naturally bad at
-                taking tests, and you can't be uninterested in doing well. You can't just take it once, and you can't
-                just take one prep course. You have to have it all—or be extremely exceptional in some aspects to make
-                up for the others. It can be quite brutal since there are so many more filters applied at these outlier
-                ranges that only candidates that meet a particular description can meet them.
-            </p>
-            <h1>A Plan and Review Approach</h1>
+                College Board’s 2015 dataset
+            </Link>. Every year, the distribution of scores resembles a normal distribution, or a bell curve, where most people score near the average and very few in the outlier ranges.</p>
+<p>I believe this conforms to a normal distribution because the requirements to score near the mean are very flexible and include many candidates. Someone can be talented at SAT tests, take it sleep deprived, and score somewhere in the middle. Someone could be naturally bad at SAT tests, do a good amount of prep, then score near the middle again. There isn't a particular description you have to fit to score near the mean since many are acceptable.</p>
+<p>But to score at the extreme outlier ranges, there isn't much leeway. I’d imagine at the extreme upper range, you can't be naturally bad at taking SAT tests, and you can't be uninterested in doing well. You can't just take it once, and you can't be bad at a single SAT topic. You have to have it all—or be extremely exceptional in some aspects to make up for the others. Only those who can fit that limited–and often brutal–description can achieve outlier scores.</p>
+<p>Looking at dice rolls can be a good analogy to give a different perspective. If we roll a single die enough times, we will get a uniform distribution of results. With two dice, it will look like a triangle because there are more ways to achieve the mean, 7, than an outlier like 2. And when we keep adding more dice, the graph quickly begins to resemble a normal distribution. There are more combinations of rolls near the middle than the ends.</p>
 
-            <p>
-                The secretary approach isn't always the most practical approach since we may be able to select a
-                candidate we previously evaluated and that it doesn't scale very well to large populations, both in
-                practicality
-                and effectiveness. But the insightful thing about it is the stopping
-                rule. By evaluating some number of candidates before deciding,
-                we can get a better understanding of the candidates a particular behavior yields. This is
-                especially important when exploring an unfamiliar field since we often have little to no
-                understanding of what makes a "good" candidate. The "review"
-                portion of the chart below is meant to help with that.
-            </p>
-            <p>
-                The "planning" portion is more of a fun exercise in my opinion. I put less weight in it since a lot of
-                it depends on chance whereas when "reviewing," you can more accurately judge your luck. But if you need
-                to be very efficient when dedicating resources to evaluate candidates, it can be
-                useful.
-            </p>
-            <SatChart/>
-            <p>
-                These simulations make two big assumptions. The population follows a distribution that the SAT
-                has and that
-                we select candidates randomly. The chart can also mislead us
-                into believing that 99 percentile means a 2220 score overall, whereas it just means compared
-                to the population our behavior is yielding. If we sample 50 pancakes at Applebee's, the best one is
-                likely to be around a
-                "2220/2400," but it's relative to pancakes made at that particular Applebee's. Relative to world, or
-                overall,
-                it probably doesn't rank as high.
-            </p>
-            <p></p>
-            <p>
-                More or less, to get a good understanding of the candidates a behavior is yielding, you probably have to
-                evaluate a decent amount of candidates. But there's a point of diminishing returns where it's unlikely
-                you will
-                find a candidate that will perform any better or worse than what you've already seen. You can decide
-                between needing to evaluate
-                more candidates, choosing a candidate, or changing your behavior to get a different distribution of
-                candidates.
-            </p>
+<DiceChart/>
 
-            <h1>Abandoning the "Numbers Game"</h1>
-            <p>
-            My personal opinion is that “playing the numbers game” can be a dangerous mindset to have since it's easy to mindlessly skim through candidates without reflecting and adapting. It’s important to evaluate a decent number of candidates, no question. But it loses meaning if no lessons are learned from why those candidates were rejected. It’s easy to go on auto-pilot and continue doing the same thing, hoping that adding more numbers will get better candidates. And sometimes that works because luck and outliers are a thing. But finding a different pool of candidates might be the more effective strategy.</p><p>
-            In the chart below, there are 3 populations being compared to each other. The two on the left are students who enrolled in CSULB for Fall 2017, and the one on the right is students enrolled (or admitted) to Harvard. The only difference between those two CSULB pools is whether or not they graduated from a high school local to the CSULB area.
-                <DistributionComparison/>
-<p>In this context, local students are given preference in admissions. If the goal is to find students who performed well in their SAT, targeting non-local students would yield better results on average. A small change in behavior can make a large difference. And a larger change in behavior, such as going to Harvard, is likely to make an even larger difference.
-</p>
-            </p>
-            <h1>Motivation</h1>
-            <p>I feel like there's a lot of pressure to find the "perfect" person to marry, the "dream" job, or the
-                "best" Node package for that obscure React app no one will look at. It has often come to a point where we
-                spend
-                more time searching than actually benefiting from the decision. But the thing is—it can be for a good reason.
-                Some of these things have huge impacts on our lives, and making sure we make the
-                right decisions is a small price to pay compared to their consequences.
-            </p>
-            <p>Part of it too, I think, is
-                that we have so many options exposed to us through technology that it makes it extremely difficult to
-                make a decision. It sometimes makes me feel nauseous when I stop to think about how much content I've
-                churned through, whereas when I was younger, I was more content with a fraction of the choices I have
-                now.
-            </p>
+<h1>Learning from the rejected candidates</h1>
+
+<p>The secretary approach isn't always the most practical approach since it’s not like we always have to evaluate and reject one candidate at a time with no going back. Real life situations can differ. But the insightful thing about it is the stopping rule. By evaluating some number of candidates before deciding, we better understand the pool of candidates we’re looking at. This is especially important when exploring an unfamiliar pool of candidates since we often have little to no understanding of what makes a "good" candidate.</p>
+<p>The chart below calculates the best candidate (from the 2015 College Board dataset) seen given the probability (AKA luck) and number of candidates evaluated. It can be used to help evaluate how good a distribution is in producing candidates. For example, with 27 candidates evaluated at random, there’s a 50% probability at least a 99 percentile (2250) candidate was found. If that candidate is not sufficient for your needs, it may be best to consider changing how you’re getting candidates.</p>
+<SatChart/>
+<h1>“Big fish, small pond”</h1>
+<p>The chart earlier is based on a perfect representation of all the SAT test takers in 2015. Sample bias is extremely common–even if we’re trying to avoid it. Our particular distribution may be very different from the actual global distribution. We can find an outlier 99 percentile candidate. But a 99 percentile candidate within a poorly performing distribution may be, say, a 70 percentile candidate globally.</p>
+<p>But before giving up on a distribution, it’s important to understand it first. Sometimes worse performing distributions are preferable. Perhaps they’re faster and more accurate to evaluate, using far less time, money, and effort. The reason can vary so much in practice, so it’s difficult to give a blanket answer.</p>
+
+<h1>Changing ponds</h1>
+<p>The benefits of finding a better distribution can be immense, and the chart below is an attempt to illustrate that. There are 3 populations being compared to each other. The yellow distribution consists of local CSULB students, who were given admission preferences, and the blue one consists of non-local students. And the crimson-red distribution consists of Harvard students.</p>
+<DistributionComparison/>
+<p>One surprising thing to me is that the blue distribution yields qualified candidates far more quickly than the yellow distribution despite it being only slightly better (arguably). My takeaway is that a small change in behavior, despite in the same setting, can make a large difference. And a larger change in behavior, such as going to Harvard, can make an unimaginable difference.</p>
+<h1>Motivation</h1>
+<p>There’s immense pressure to always seek the best out there, whether a partner, job, house, or Node package for that obscure React app no one will look at. It’s time consuming, frustrating, and stressful. Sometimes you invest so much into a candidate or distribution of candidates, and it doesn’t work out. It’s difficult, and I feel like many people settle or give-up on unideal candidates due to fatigue and overload of decisions.</p>
+<p>I encountered the secretary problem mid-2020 (with, presumably, a lot of sudden free time on my hands) from a Reddit post. I found it incredibly relevant and useful, especially given how many choices we have nowadays, ranging from food/products with online delivery, dating apps, and Node packages.</p>
+<p>So this article is just a state of my perspective on dealing with this issue. I tend to write things out when I need to think. Not sure how valid this whole approach and perspective is, and I don’t think I’ll ever have enough life experience to make an absolute statement on how things should be done.</p>
 
         </Typography>
     );
